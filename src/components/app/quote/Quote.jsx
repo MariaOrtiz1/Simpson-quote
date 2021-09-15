ae318b6583e593b5f6251330adae50521a4b2a7d
Maria Ortiz-Lopez <OrtizLopez.Maria030@gmail.com>
2021-09-15 12:32:57 -0700

    Added in SimpsonsQuote.jsx file

--- src/components/app/quote/Quote.jsx
+++ src/components/app/quote/Quote.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+// This is the display, how it is going to look like after we load it 
 const Quote = ({ name, image, quote }) => (
   <figure>
     <img src={image} alt={name} />
---
+++ src/containers/SimpsonsQuote.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import Load from '../components/quote/Load';
+import Quote from '../components/quote/Quote';
+import { fetchQuote } from '../services/SimpsonsApi';
+
+const SimpsonsQuote = () => {
+  const [quote, setQuote] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  // eslint-disable-next-line max-len
+  // On click, this fuction will change the Loading state to true, allowing it to render the loading text. Afterwards it will fetch a quote, and render it as stated within our Quote template, then set the Loading state to false that that it will go away.
+  const handleClick = async () => {
+    setLoading(true);
+    const quote = await fetchQuote();
+    setQuote(quote);
+    setLoading(false);
+  };
+
+  return (
+    <>
+      <Load onClick={handleClick} />
+      {/* If loading is true, then load what is after the '?'  */}
+      {loading ? (
+        <h1>Loading now!</h1>
+      ) : (
+        quote && (
+          <Quote name={quote.name} image={quote.image} quote={quote.quote} />
+        )
+      )}
+    </>
+  );
+};
+
+
+export default SimpsonsQuote;
